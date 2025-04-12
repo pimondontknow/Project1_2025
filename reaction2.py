a703afc81d5fa69905d4f4916f7755f17651ea1d
unknown <1607629453@qq.com>
2025-04-12 14:21:01 +0800

    Final tasks

--- reaction2.py
+++ reaction2.py
@@ -1,20 +1,75 @@
 from gpiozero import LED, Button
-from time import sleep
+from time import sleep, time
 from random import uniform
+
 led = LED(4)
-right_button=Button(15)
-left_button=Button(14)
-left_name=input('left player name is')
-right_name=input('right player name is')
-led.on()
-sleep(uniform(5, 10))
-led.off()
+right_button = Button(15)
+left_button = Button(14)
+
+left_name = input('Left player name: ')
+right_name = input('Right player name: ')
+left_score = 0
+right_score = 0
+round_count = 0
+start_time = 0
+game_active = False
+
+def play_round():
+    global start_time, game_active, round_count
+    round_count += 1
+    
+    print(f"\n--- Round {round_count}/5 ---")
+    print(f"{left_name}: {left_score}  {right_name}: {right_score}")
+    
+    led.on()
+    delay = uniform(5, 10)
+    sleep(delay)
+    led.off()
+    
+    start_time = time()
+    game_active = True
+
 def pressed(button):
-print(str(button.pin.number) + ' won the game')
+    global left_score, right_score, game_active
+    if not game_active:
+        return
+    
+    reaction_time = time() - start_time
+    game_active = False
+    
+    if reaction_time < 0:
+        print("Invalid press! Pressed too early!")
+        return
+    
+    winner = left_name if button.pin.number == 14 else right_name
+    print(f"{winner} wins! Reaction time: {reaction_time:.2f} seconds")
+    
+    if button.pin.number == 14:
+        left_score += 1
+    else:
+        right_score += 1
+
+
 right_button.when_pressed = pressed
 left_button.when_pressed = pressed
-if button.pin.number == 14:
-print(left_name + ' won the game')
+
+for _ in range(5):
+    play_round()
+    while game_active: 
+        sleep(0.1)
+    sleep(1)  
+
+print("\n=== Final Result ===")
+print(f"{left_name}: {left_score}  {right_name}: {right_score}")
+
+if left_score > right_score:
+    print(f"{left_name} is the ultimate winner!")
+elif right_score > left_score:
+    print(f"{right_name} is the ultimate winner!")
 else:
-print(right_name + ' won the game')
-exit() 
+    print("It's a tie!")
+
+led.off()
+right_button.close()
+left_button.close()
+ 
