e3b7106d250f7f80633dd723ccfa26523f8ec65a
VulcaN <127948316+pimondontknow@users.noreply.github.com>
2025-04-12 22:05:52 +0800

    final version

--- reaction.py
+++ reaction.py
@@ -3,28 +3,45 @@ from time import sleep
 from random import uniform
 
 led = LED(4)
+
 right_button = Button(15)
 left_button = Button(14)
 
 left_name = input('Left player name: ')
 right_name = input('Right player name: ')
 
-def play_game():
+ def play_round():
+    global  game_active, round_count
+    round_count += 1
+    
+    print(f"\n--- Round {round_count}/5 ---")
+    
     led.on()
     sleep(uniform(5, 10))
     led.off()
 
+    game_active = True
+
 def pressed(button):
-    if button.pin.number == 14:
+    global  game_active
+    if not game_active:
+        return
+
+    game_active = False 
+if button.pin.number == 14:
         print(f"{left_name} won the game!")
     else:
         print(f"{right_name} won the game!")
 
-    right_button.when_pressed = None
-    left_button.when_pressed = None
+right_button.when_pressed = pressed
+left_button.when_pressed = pressed
 
-while True:
-    right_button.when_pressed = pressed
-    left_button.when_pressed = pressed
-    play_game()
+for _ in range(5):
+    play_round()
+    while game_active: 
+        sleep(0.1)
     sleep(1)
+
+led.off()
+right_button.close()
+left_button.close()
